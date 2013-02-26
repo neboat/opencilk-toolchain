8b9db2b79f9975c4c11091a648cbf9090684177e
Sylvestre Ledru <sylvestre@debian.org>
2013-02-26 16:34:08 +0000

    * Draft of a snapshot release (3.3)

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -1,23 +1,46 @@
 #!/bin/sh
+# This script will create the following tarballs:
+# llvm-toolchain-3.2_3.2repack.orig-clang.tar.bz2
+# llvm-toolchain-3.2_3.2repack.orig-compiler-rt.tar.bz2
+# llvm-toolchain-3.2_3.2repack.orig-lldb.tar.bz2
+# llvm-toolchain-3.2_3.2repack.orig-polly.tar.bz2
+# llvm-toolchain-3.2_3.2repack.orig.tar.bz2
 
-# called by uscan with '--upstream-version' <version> <file>
-DIR=clang-*.src
-DIRIN=clang-$2
-TAR=../clang_$2.orig.tar.bz2
+SVN_URL=http://llvm.org/svn/llvm-project/
+REVISION=$(LANG=C svn info $SVN_URL|grep "^Revision:"|awk '{print $2}')
+SVN_CMD="svn export -r $REVISION $SVN_URL"
+MAJOR_VERSION=3.3
 
-# clean up the upstream tarball
-tar zxvf $3
-mkdir -p $DIRIN/tools/clang
-mv $DIR/* $DIRIN/tools/clang/
-tar -c -j -f $TAR $DIRIN
-rm -rf $DIRIN $DIR
+# LLVM
+LLVM_TARGET=llvm-toolchain_3.3~svn$REVISION
+$SVN_CMD/llvm/trunk $LLVM_TARGET
+tar jcvf llvm-toolchain_3.3~svn$REVISION.orig.tar.bz2 $LLVM_TARGET
+rm -rf $LLVM_TARGET
 
-# move to directory 'tarballs'
-if [ -r .svn/deb-layout ]; then
-    . .svn/deb-layout
-    mv $TAR $origDir
-    echo "moved $TAR to $origDir"
-fi
+# Clang
+CLANG_TARGET=clang_3.3~svn$REVISION
+$SVN_CMD/cfe/trunk $CLANG_TARGET
+tar jcvf llvm-toolchain_3.3~svn$REVISION.orig-clang.tar.bz2 $CLANG_TARGET
+rm -rf $CLANG_TARGET
+
+# Compiler-rt
+COMPILER_RT_TARGET=compiler-rt_3.3~svn$REVISION
+$SVN_CMD/compiler-rt/trunk $COMPILER_RT_TARGET
+tar jcvf llvm-toolchain_3.3~svn$REVISION.orig-compiler-rt.tar.bz2 $COMPILER_RT_TARGET
+rm -rf $COMPILER_RT_TARGET
+
+# Polly
+POLLY_TARGET=polly_3.3~svn$REVISION
+$SVN_CMD/polly/trunk $POLLY_TARGET
+tar jcvf llvm-toolchain_3.3~svn$REVISION.orig-polly.tar.bz2 $POLLY_TARGET
+rm -rf $POLLY_TARGET
+
+# LLDB
+LLDB_TARGET=lldb_3.3~svn$REVISION
+$SVN_CMD/lldb/trunk $LLDB_TARGET
+tar jcvf llvm-toolchain_3.3~svn$REVISION.orig-lldb.tar.bz2 $LLDB_TARGET
+rm -rf $LLDB_TARGET
+
 
 exit 0
 
--- debian/prepare-new-release.sh
+++ debian/prepare-new-release.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-ORIG_VERSION=3.1
-TARGET_VERSION=3.2
-ORIG_VERSION_2=3_1
-TARGET_VERSION_2=3_2
+ORIG_VERSION=3.2
+TARGET_VERSION=3.3
+ORIG_VERSION_2=3_2
+TARGET_VERSION_2=3_3
 
 LIST=`ls debian/*$ORIG_VERSION*`
 for F in $LIST; do
     TARGET=`echo $F|sed -e "s|$ORIG_VERSION|$TARGET_VERSION|g"`
-    mv $F $TARGET
+    svn mv $F $TARGET
 done
 LIST=`ls debian/*$TARGET_VERSION* debian/control`
 for F in $LIST; do
