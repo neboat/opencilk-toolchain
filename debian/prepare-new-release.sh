6592fa9d5231cd20fa29b689dce16eae78ad8407
Sylvestre Ledru <sylvestre@debian.org>
2013-02-26 16:40:09 +0000

    Update the upgrade script

--- debian/prepare-new-release.sh
+++ debian/prepare-new-release.sh
@@ -9,7 +9,7 @@ for F in $LIST; do
     TARGET=`echo $F|sed -e "s|$ORIG_VERSION|$TARGET_VERSION|g"`
     svn mv $F $TARGET
 done
-LIST=`ls debian/*$TARGET_VERSION* debian/control`
+LIST=`ls debian/*$TARGET_VERSION* debian/control debian/*.install debian/*.links`
 for F in $LIST; do
     sed -i -e "s|$ORIG_VERSION_2|$TARGET_VERSION_2|g" $F
     sed -i -e "s|$ORIG_VERSION|$TARGET_VERSION|g" $F
