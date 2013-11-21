1ed1331ccb4cae25eaf2dcb3ecdd0a62f10f9a64
Sylvestre Ledru <sylvestre@debian.org>
2013-11-21 07:25:06 +0000

    Improve the repack script to generate the rc1 tarballs

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -9,19 +9,32 @@
 set -e
 
 SVN_BASE_URL=http://llvm.org/svn/llvm-project/
+MAJOR_VERSION=3.4
 
 if test -n "$1"; then
 # http://llvm.org/svn/llvm-project/{cfe,llvm,compiler-rt,...}/branches/google/stable/
     BRANCH=$1
 fi
 
+if test -n "$1" -a -n "$2"; then
+# http://llvm.org/svn/llvm-project/{cfe,llvm,compiler-rt,...}/branches/google/stable/
+    BRANCH=$1
+    TAG=$2
+    RCRELEASE="true"
+fi
+
 get_svn_url() {
     MODULE=$1
     BRANCH=$2
-    if test -n "$BRANCH"; then
-        SVN_URL="$SVN_BASE_URL/$MODULE/branches/$BRANCH"
+    TAG=$3
+    if test -n "$TAG"; then
+            SVN_URL="$SVN_BASE_URL/$MODULE/tags/$BRANCH/$TAG"
     else
-        SVN_URL="$SVN_BASE_URL/$MODULE/trunk/"
+        if test -n "$BRANCH"; then
+            SVN_URL="$SVN_BASE_URL/$MODULE/branches/$BRANCH"
+        else
+            SVN_URL="$SVN_BASE_URL/$MODULE/trunk/"
+        fi
     fi
     echo $SVN_URL
 }
@@ -30,7 +43,7 @@ get_higher_revision() {
     PROJECTS="llvm cfe compiler-rt polly lldb clang-tools-extra"
     REVISION_MAX=0
     for f in $PROJECTS; do
-        REVISION=$(LANG=C svn info $(get_svn_url $f $BRANCH)|grep "^Last Changed Rev:"|awk '{print $4}')
+        REVISION=$(LANG=C svn info $(get_svn_url $f $BRANCH $TAG)|grep "^Last Changed Rev:"|awk '{print $4}')
         if test $REVISION -gt $REVISION_MAX; then
             REVISION_MAX=$REVISION
         fi
@@ -50,46 +63,52 @@ else
     SVN_CMD="svn export -r $REVISION"
 fi
 
-MAJOR_VERSION=3.4
+if test -n "$RCRELEASE"; then
+    VERSION=$MAJOR_VERSION~+$TAG
+    FULL_VERSION=llvm-toolchain-$MAJOR_VERSION_$MAJOR_VERSION~+$TAG
+else
+    VERSION=$MAJOR_VERSION~svn$REVISION
+    FULL_VERSION=llvm-toolchain-snapshot_$MAJOR_VERSION~svn$REVISION
+fi
 
 # LLVM
-LLVM_TARGET=llvm-toolchain-snapshot_$MAJOR_VERSION~svn$REVISION
-$SVN_CMD $(get_svn_url llvm $BRANCH) $LLVM_TARGET
-tar jcvf llvm-toolchain-snapshot_$MAJOR_VERSION~svn$REVISION.orig.tar.bz2 $LLVM_TARGET
+LLVM_TARGET=$FULL_VERSION
+$SVN_CMD $(get_svn_url llvm $BRANCH $TAG) $LLVM_TARGET
+tar jcvf $FULL_VERSION.orig.tar.bz2 $LLVM_TARGET
 rm -rf $LLVM_TARGET
 
 
 # Clang
-CLANG_TARGET=clang_$MAJOR_VERSION~svn$REVISION
-$SVN_CMD $(get_svn_url cfe $BRANCH) $CLANG_TARGET
-tar jcvf llvm-toolchain-snapshot_$MAJOR_VERSION~svn$REVISION.orig-clang.tar.bz2 $CLANG_TARGET
+CLANG_TARGET=clang_$VERSION
+$SVN_CMD $(get_svn_url cfe $BRANCH $TAG) $CLANG_TARGET
+tar jcvf $FULL_VERSION.orig-clang.tar.bz2 $CLANG_TARGET
 rm -rf $CLANG_TARGET
 
 
 # Clang extra
-CLANG_TARGET=clang-tools-extra_$MAJOR_VERSION~svn$REVISION
-$SVN_CMD $(get_svn_url clang-tools-extra $BRANCH) $CLANG_TARGET
-tar jcvf llvm-toolchain-snapshot_$MAJOR_VERSION~svn$REVISION.orig-clang-tools-extra.tar.bz2 $CLANG_TARGET
+CLANG_TARGET=clang-tools-extra_$VERSION
+$SVN_CMD $(get_svn_url clang-tools-extra $BRANCH $TAG) $CLANG_TARGET
+tar jcvf $FULL_VERSION.orig-clang-tools-extra.tar.bz2 $CLANG_TARGET
 rm -rf $CLANG_TARGET
 
 # Compiler-rt
-COMPILER_RT_TARGET=compiler-rt_$MAJOR_VERSION~svn$REVISION
-$SVN_CMD $(get_svn_url compiler-rt $BRANCH) $COMPILER_RT_TARGET
-tar jcvf llvm-toolchain-snapshot_$MAJOR_VERSION~svn$REVISION.orig-compiler-rt.tar.bz2 $COMPILER_RT_TARGET
+COMPILER_RT_TARGET=compiler-rt_$VERSION
+$SVN_CMD $(get_svn_url compiler-rt $BRANCH $TAG) $COMPILER_RT_TARGET
+tar jcvf $FULL_VERSION.orig-compiler-rt.tar.bz2 $COMPILER_RT_TARGET
 rm -rf $COMPILER_RT_TARGET
 
 # Polly
-POLLY_TARGET=polly_$MAJOR_VERSION~svn$REVISION
-$SVN_CMD $(get_svn_url polly $BRANCH) $POLLY_TARGET
+POLLY_TARGET=polly_$VERSION
+$SVN_CMD $(get_svn_url polly $BRANCH $TAG) $POLLY_TARGET
 rm -rf $POLLY_TARGET/www
-tar jcvf llvm-toolchain-snapshot_$MAJOR_VERSION~svn$REVISION.orig-polly.tar.bz2 $POLLY_TARGET
+tar jcvf $FULL_VERSION.orig-polly.tar.bz2 $POLLY_TARGET
 rm -rf $POLLY_TARGET
 
 # LLDB
-LLDB_TARGET=lldb_$MAJOR_VERSION~svn$REVISION
-$SVN_CMD $(get_svn_url lldb $BRANCH) $LLDB_TARGET
+LLDB_TARGET=lldb_$VERSION
+$SVN_CMD $(get_svn_url lldb $BRANCH $TAG) $LLDB_TARGET
 rm -rf $LLDB_TARGET/www/
-tar jcvf llvm-toolchain-snapshot_$MAJOR_VERSION~svn$REVISION.orig-lldb.tar.bz2 $LLDB_TARGET
+tar jcvf $FULL_VERSION.orig-lldb.tar.bz2 $LLDB_TARGET
 rm -rf $LLDB_TARGET
 
 PATH_DEBIAN="$(pwd)/$(dirname $0)/../"
@@ -101,6 +120,6 @@ cd $PATH_DEBIAN
 if test -z "$DISTRIBUTION"; then
     DISTRIBUTION="experimental"
 fi
-dch --distribution $DISTRIBUTION --newversion 1:$MAJOR_VERSION~svn$REVISION-1~exp1 "New snapshot release"
+dch --distribution $DISTRIBUTION --newversion 1:$VERSION-1~exp1 "New snapshot release"
 
 exit 0
