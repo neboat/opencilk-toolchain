b26993a5a3668310e044fa68e9915e91e3e34e93
Pablo Oliveira <pablo@sifflez.org>
2016-06-10 07:54:38 +0000

    Update prepare-new-release.sh

--- debian/prepare-new-release.sh
+++ debian/prepare-new-release.sh
@@ -9,7 +9,7 @@ for F in $LIST; do
     TARGET=`echo $F|sed -e "s|$ORIG_VERSION|$TARGET_VERSION|g"`
     svn mv $F $TARGET
 done
-LIST=`ls debian/*$TARGET_VERSION* debian/control debian/*.install debian/*.links debian/orig-tar.sh debian/rules debian/patches/clang-analyzer-force-version.diff debian/patches/clang-format-version.diff debian/patches/python-clangpath.diff debian/patches/scan-build-clang-path.diff`
+LIST=`ls debian/*$TARGET_VERSION* debian/control debian/*.install debian/*.links debian/orig-tar.sh debian/rules debian/patches/clang-analyzer-force-version.diff debian/patches/clang-format-version.diff debian/patches/python-clangpath.diff debian/patches/scan-build-clang-path.diff debian/patches/lldb-libname.diff`
 for F in $LIST; do
     sed -i -e "s|$ORIG_VERSION_2|$TARGET_VERSION_2|g" $F
     sed -i -e "s|$ORIG_VERSION|$TARGET_VERSION|g" $F
