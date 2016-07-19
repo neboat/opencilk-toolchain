5eed75ad17798474a0a78fc8da5a28b2ad974cc6
Sylvestre Ledru <sylvestre@debian.org>
2016-07-19 12:23:09 +0000

    remove deprecated changes from the update script

--- debian/prepare-new-release.sh
+++ debian/prepare-new-release.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
-ORIG_VERSION=3.8
-TARGET_VERSION=3.9
-ORIG_VERSION_2=3_8
-TARGET_VERSION_2=3_9
+ORIG_VERSION=3.9
+TARGET_VERSION=4.0
+ORIG_VERSION_2=3_9
+TARGET_VERSION_2=4_0
 
-LIST=`ls debian/*$ORIG_VERSION*`
-for F in $LIST; do
-    TARGET=`echo $F|sed -e "s|$ORIG_VERSION|$TARGET_VERSION|g"`
-    svn mv $F $TARGET
-done
-LIST=`ls debian/*$TARGET_VERSION* debian/control debian/*.install debian/*.links debian/orig-tar.sh debian/rules debian/patches/clang-analyzer-force-version.diff debian/patches/clang-format-version.diff debian/patches/python-clangpath.diff debian/patches/scan-build-clang-path.diff debian/patches/lldb-libname.diff`
+LIST=`ls debian/control debian/orig-tar.sh debian/rules debian/patches/clang-analyzer-force-version.diff debian/patches/clang-format-version.diff debian/patches/python-clangpath.diff debian/patches/scan-build-clang-path.diff debian/patches/lldb-libname.diff`
 for F in $LIST; do
     sed -i -e "s|$ORIG_VERSION_2|$TARGET_VERSION_2|g" $F
     sed -i -e "s|$ORIG_VERSION|$TARGET_VERSION|g" $F
