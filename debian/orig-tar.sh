71a5512f39b9b4a84c126df6a1a9ea6d423fd288
Sylvestre Ledru <sylvestre@debian.org>
2016-09-01 11:48:01 +0000

    remove the word final from the tarball generation

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -104,8 +104,11 @@ else
 fi
 
 if test -n "$RCRELEASE"; then
-#    VERSION=$MAJOR_VERSION"+"$REVISION # WAS TAG
-    VERSION=$MAJOR_VERSION"~+"$TAG
+    if test "$TAG" = "final"; then
+	VERSION=$MAJOR_VERSION
+    else
+        VERSION=$MAJOR_VERSION"~+"$TAG
+    fi
     FULL_VERSION="llvm-toolchain-"$MAJOR_VERSION"_"$VERSION
 else
     VERSION=$CURRENT_VERSION"~svn"$REVISION
