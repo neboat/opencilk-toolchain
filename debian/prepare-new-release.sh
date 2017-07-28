14851313d7597d0951e66921f6d19ccb376b6020
Sylvestre Ledru <sylvestre@debian.org>
2017-07-28 06:30:24 +0000

    Also update clang-apply-replacements.diff

--- debian/prepare-new-release.sh
+++ debian/prepare-new-release.sh
@@ -4,7 +4,7 @@ TARGET_VERSION=6.0
 ORIG_VERSION_2=6_0
 TARGET_VERSION_2=6_0
 
-LIST=`ls debian/control debian/orig-tar.sh debian/rules debian/patches/clang-analyzer-force-version.diff debian/patches/clang-format-version.diff debian/patches/python-clangpath.diff debian/patches/scan-build-clang-path.diff debian/patches/lldb-libname.diff debian/patches/fix-scan-view-path.diff debian/patches/lldb-addversion-suffix-to-llvm-server-exec.patch debian/patches/clang-tidy-run-bin.diff`
+LIST=`ls debian/control debian/orig-tar.sh debian/rules debian/patches/clang-analyzer-force-version.diff debian/patches/clang-format-version.diff debian/patches/python-clangpath.diff debian/patches/scan-build-clang-path.diff debian/patches/lldb-libname.diff debian/patches/fix-scan-view-path.diff debian/patches/lldb-addversion-suffix-to-llvm-server-exec.patch debian/patches/clang-tidy-run-bin.diff debian/patches/clang-apply-replacements.diff`
 for F in $LIST; do
     sed -i -e "s|$ORIG_VERSION_2|$TARGET_VERSION_2|g" $F
     sed -i -e "s|$ORIG_VERSION|$TARGET_VERSION|g" $F
