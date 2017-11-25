0ee688cf909d8fc542deab5f1bd54bd70ee034d9
Sylvestre Ledru <sylvestre@debian.org>
2017-11-25 14:19:02 +0000

    Fix the fix-scan-view-path.diff path

--- debian/prepare-new-release.sh
+++ debian/prepare-new-release.sh
@@ -4,7 +4,7 @@ TARGET_VERSION=5.0
 ORIG_VERSION_2=4_0
 TARGET_VERSION_2=5_0
 
-LIST=`ls debian/control debian/orig-tar.sh debian/rules debian/patches/clang-analyzer-force-version.diff debian/patches/clang-format-version.diff debian/patches/python-clangpath.diff debian/patches/scan-build-clang-path.diff debian/patches/lldb-libname.diff debian/patches/fix-scan-view-path.diff debian/patches/lldb-addversion-suffix-to-llvm-server-exec.patch debian/patches/clang-tidy-run-bin.diff`
+LIST=`ls debian/control debian/orig-tar.sh debian/rules debian/patches/clang-analyzer-force-version.diff debian/patches/clang-format-version.diff debian/patches/python-clangpath.diff debian/patches/scan-build-clang-path.diff debian/patches/lldb-libname.diff debian/patches/fix-scan-view-path.diff debian/patches/lldb-addversion-suffix-to-llvm-server-exec.patch debian/patches/clang-tidy-run-bin.diff debian/patches/fix-scan-view-path.diff`
 for F in $LIST; do
     sed -i -e "s|$ORIG_VERSION_2|$TARGET_VERSION_2|g" $F
     sed -i -e "s|$ORIG_VERSION|$TARGET_VERSION|g" $F
