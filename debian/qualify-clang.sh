913c0db72dd5ed85bfa47fd7d54679d5daf87260
Sylvestre Ledru <sylvestre@debian.org>
2018-08-15 22:24:06 +0200

    also test clang++-libc++

--- debian/qualify-clang.sh
+++ debian/qualify-clang.sh
@@ -239,6 +239,9 @@ int main() {
 clang++-$VERSION -std=c++17 -stdlib=libc++ foo.cpp -lc++experimental -lc++fs -o o
 ./o > /dev/null
 
+clang++-libc++ -std=c++17 foo.cpp -lc++experimental -lc++fs -o o
+./o > /dev/null
+
 g++ -nostdinc++ -I/usr/lib/llvm-7/bin/../include/c++/v1/ -L/usr/lib/llvm-$VERSION/lib/ \
     foo.cpp -nodefaultlibs -std=c++17 -lc++ -lc++abi -lm -lc -lgcc_s -lgcc
 ./o > /dev/null
