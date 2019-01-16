be346973e54b7477a5a86a3a9a722084a8f73ca2
Sylvestre Ledru <sylvestre@debian.org>
2019-01-16 15:39:16 +0100

    remove qualify-clang from the list of version

--- debian/prepare-new-release.sh
+++ debian/prepare-new-release.sh
@@ -6,7 +6,7 @@ TARGET_VERSION_2=8_0
 ORIG_VERSION_3=70
 TARGET_VERSION_3=80
 
-LIST=`ls debian/control debian/orig-tar.sh debian/rules debian/patches/clang-analyzer-force-version.diff debian/patches/clang-format-version.diff debian/patches/python-clangpath.diff debian/patches/scan-build-clang-path.diff debian/patches/lldb-libname.diff debian/patches/fix-scan-view-path.diff debian/patches/lldb-addversion-suffix-to-llvm-server-exec.patch debian/patches/clang-tidy-run-bin.diff debian/patches/fix-scan-view-path.diff debian/qualify-clang.sh debian/README debian/patches/clang-analyzer-force-version.diff debian/patches/clang-tidy-run-bin.diff debian/tests/control`
+LIST=`ls debian/control debian/orig-tar.sh debian/rules debian/patches/clang-analyzer-force-version.diff debian/patches/clang-format-version.diff debian/patches/python-clangpath.diff debian/patches/scan-build-clang-path.diff debian/patches/lldb-libname.diff debian/patches/fix-scan-view-path.diff debian/patches/lldb-addversion-suffix-to-llvm-server-exec.patch debian/patches/clang-tidy-run-bin.diff debian/patches/fix-scan-view-path.diff debian/README debian/patches/clang-analyzer-force-version.diff debian/patches/clang-tidy-run-bin.diff debian/tests/control`
 for F in $LIST; do
     sed -i -e "s|$ORIG_VERSION_3|$TARGET_VERSION_3|g" $F
     sed -i -e "s|$ORIG_VERSION_2|$TARGET_VERSION_2|g" $F
