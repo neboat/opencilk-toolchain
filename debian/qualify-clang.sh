3c06c08d3cc24890c02cd7a7767cd0c5efe10d11
Sylvestre Ledru <sylvestre@debian.org>
2019-06-08 22:29:03 +0200

    exegesis: fix the test

--- debian/qualify-clang.sh
+++ debian/qualify-clang.sh
@@ -320,8 +320,8 @@ if ! grep "foo.cpp:3:3" foo.log; then
     exit 42
 fi
 
-echo "vzeroupper" | llvm-exegesis-$VERSION -mode=uops -snippets-file=- &> foo.log
-if ! grep "measurements:" foo.log; then
+echo "vzeroupper" | llvm-exegesis-$VERSION -mode=uops -snippets-file=- &> foo.log || true
+if grep -q -E "(built without libpfm|cannot initialize libpfm)" foo.log; then
     echo "could not run llvm-exegesis correctly"
     cat foo.log
     exit 42
