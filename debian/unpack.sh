38d4c89035e716c83d8c4f4a3c55c7a790938992
Sylvestre Ledru <sledru@mozilla.com>
2019-10-20 18:40:16 +0200

    unpack script for 9

--- debian/unpack.sh
+++ debian/unpack.sh
@@ -1,28 +1,16 @@
 set -e
 ORIG_VERSION=9
 MAJOR_VERSION=9 # 8.0.1
-SVN_REV=`ls -1 *9_$MAJOR_VERSION*svn*bz2 | tail -1|perl -ne 'print "$1\n" if /svn(\d+)/;' | sort -ru`
+REV=`ls -1 *$ORIG_VERSION_$MAJOR_VERSION*~+*xz | tail -1|perl -ne 'print "$1\n" if /~\+(.*)\.orig/;'  | sort -ru`
+
 #SVN_REV=347285
-VERSION=svn$SVN_REV
+VERSION=$REV
+echo $VERSION
 #VERSION=+rc3
-LLVM_ARCHIVE=llvm-toolchain-${ORIG_VERSION}_$MAJOR_VERSION~$VERSION.orig.tar.bz2
+LLVM_ARCHIVE=llvm-toolchain-${ORIG_VERSION}_$MAJOR_VERSION~+$VERSION.orig.tar.xz
 echo "unpack of $LLVM_ARCHIVE"
-tar jxf $LLVM_ARCHIVE
-cd llvm-toolchain-${ORIG_VERSION}_$MAJOR_VERSION~$VERSION/ || ( echo "Bad SVN_REV:\"$SVN_REV\"" && exit 1 )
-for f in ../llvm-toolchain-${ORIG_VERSION}_$MAJOR_VERSION~$VERSION.orig-clang.tar.bz2 ../llvm-toolchain-${ORIG_VERSION}_$MAJOR_VERSION~$VERSION.orig-clang-tools-extra.tar.bz2 ../llvm-toolchain-${ORIG_VERSION}_$MAJOR_VERSION~$VERSION.orig-compiler-rt.tar.bz2 ../llvm-toolchain-${ORIG_VERSION}_$MAJOR_VERSION~$VERSION.orig-lld.tar.bz2 ../llvm-toolchain-${ORIG_VERSION}_$MAJOR_VERSION~$VERSION.orig-lldb.tar.bz2 ../llvm-toolchain-${ORIG_VERSION}_$MAJOR_VERSION~$VERSION.orig-polly.tar.bz2 ../llvm-toolchain-${ORIG_VERSION}_$MAJOR_VERSION~$VERSION.orig-libcxxabi.tar.bz2 ../llvm-toolchain-${ORIG_VERSION}_$MAJOR_VERSION~$VERSION.orig-libcxx.tar.bz2 ../llvm-toolchain-${ORIG_VERSION}_$MAJOR_VERSION~$VERSION.orig-openmp.tar.bz2; do
-	echo "Unpack of $f"
- 	tar jxf $f
- done
+tar Jxf $LLVM_ARCHIVE
+cd llvm-toolchain-${ORIG_VERSION}_$MAJOR_VERSION~+$VERSION/
 
-ln -s clang_$MAJOR_VERSION~$VERSION clang
-ln -s clang-tools-extra_$MAJOR_VERSION~$VERSION clang-tools-extra
-ln -s compiler-rt_$MAJOR_VERSION~$VERSION compiler-rt
-ln -s polly_$MAJOR_VERSION~$VERSION polly
-ln -s lldb_$MAJOR_VERSION~$VERSION lldb
-ln -s lld_$MAJOR_VERSION~$VERSION lld
-ln -s openmp_$MAJOR_VERSION~$VERSION openmp
-ln -s libcxx_$MAJOR_VERSION~$VERSION libcxx
-ln -s libcxxabi_$MAJOR_VERSION~$VERSION libcxxabi
-
-cp -R ../9/debian .
+cp -R ../snapshot/debian .
 QUILT_PATCHES=debian/patches/ quilt push -a --fuzz=0
