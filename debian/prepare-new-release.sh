cf9a0de75bbf58f0fa936da7cf8ef2b99f05997f
Sylvestre Ledru <sylvestre@debian.org>
2020-07-15 17:25:58 +0200

    prepare -12

--- debian/prepare-new-release.sh
+++ debian/prepare-new-release.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-ORIG_VERSION=9
-TARGET_VERSION=10
-ORIG_VERSION_2=9_0
-TARGET_VERSION_2=10_0
-ORIG_VERSION_3=90
-TARGET_VERSION_3=100
+ORIG_VERSION=11
+TARGET_VERSION=12
+ORIG_VERSION_2=11_0
+TARGET_VERSION_2=12_0
+ORIG_VERSION_3=110
+TARGET_VERSION_3=120
 
 LIST=`ls debian/control debian/orig-tar.sh debian/rules debian/patches/clang-analyzer-force-version.diff debian/patches/clang-format-version.diff debian/patches/python-clangpath.diff debian/patches/scan-build-clang-path.diff debian/patches/lldb-libname.diff debian/patches/fix-scan-view-path.diff debian/patches/lldb/lldb-addversion-suffix-to-llvm-server-exec.patch debian/patches/clang-tidy-run-bin.diff debian/patches/fix-scan-view-path.diff debian/README debian/patches/clang-analyzer-force-version.diff debian/patches/clang-tidy-run-bin.diff debian/tests/control debian/unpack.sh`
 for F in $LIST; do
