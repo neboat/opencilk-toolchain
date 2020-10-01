92c48ba3c1eb84b0fa669bccd4dc9b70f6a758d6
Gianfranco Costamagna <costamagnagianfranco@yahoo.it>
2020-10-01 15:19:43 +0200

    Reduce the string comparison for z3 support

--- debian/qualify-clang.sh
+++ debian/qualify-clang.sh
@@ -387,7 +387,7 @@ void testBitwiseRules(unsigned int a, int b) {
 ' > foo.c
 
 clang-$VERSION -cc1  -analyze -analyzer-constraints=range -analyzer-checker=core,debug.ExprInspection -analyzer-constraints=z3 foo.c &> foo.log
-if ! grep -q "fatal error: error in backend: LLVM was not compiled with Z3 support, rebuild with -DLLVM_ENABLE_Z3_SOLVER=ON" foo.log; then
+if ! grep -q "LLVM was not compiled with Z3 support" foo.log; then
     # Should work
     clang-$VERSION -cc1  -analyze -analyzer-constraints=range -analyzer-checker=core,debug.ExprInspection -verify -analyzer-config eagerly-assume=false -analyzer-constraints=z3 foo.c
     clang-$VERSION -cc1  -analyze -analyzer-constraints=range -analyzer-checker=core,debug.ExprInspection -analyzer-constraints=z3 foo.c &> foo.log
