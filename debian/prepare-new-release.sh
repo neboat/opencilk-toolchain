7881e45616705cfadd6b91cdc9509608526d2320
Sylvestre Ledru <sylvestre@debian.org>
2021-06-22 23:09:41 +0200

    Fix the path to analyze-{cc,c++} for scan-build-py with --intercept-first (scan-build-py-fix-analyze-path.diff)

--- debian/prepare-new-release.sh
+++ debian/prepare-new-release.sh
@@ -6,7 +6,7 @@ TARGET_VERSION_2=12_0
 ORIG_VERSION_3=110
 TARGET_VERSION_3=120
 
-LIST=`ls debian/control debian/orig-tar.sh debian/rules debian/patches/clang-analyzer-force-version.diff debian/patches/clang-format-version.diff debian/patches/python-clangpath.diff debian/patches/scan-build-clang-path.diff debian/patches/lldb-libname.diff debian/patches/fix-scan-view-path.diff debian/patches/lldb/lldb-addversion-suffix-to-llvm-server-exec.patch debian/patches/clang-tidy-run-bin.diff debian/patches/fix-scan-view-path.diff debian/README debian/patches/clang-analyzer-force-version.diff debian/patches/clang-tidy-run-bin.diff debian/tests/control debian/unpack.sh debian/tests/cmake-test`
+LIST=`ls debian/control debian/orig-tar.sh debian/rules debian/patches/clang-analyzer-force-version.diff debian/patches/clang-format-version.diff debian/patches/python-clangpath.diff debian/patches/scan-build-clang-path.diff debian/patches/lldb-libname.diff debian/patches/fix-scan-view-path.diff debian/patches/lldb/lldb-addversion-suffix-to-llvm-server-exec.patch debian/patches/clang-tidy-run-bin.diff debian/patches/fix-scan-view-path.diff debian/README debian/patches/clang-analyzer-force-version.diff debian/patches/clang-tidy-run-bin.diff debian/tests/control debian/unpack.sh debian/tests/cmake-test debian/patches/scan-build-py-fix-analyze-path.diff`
 for F in $LIST; do
     sed -i -e "s|$ORIG_VERSION_3|$TARGET_VERSION_3|g" $F
     sed -i -e "s|$ORIG_VERSION_2|$TARGET_VERSION_2|g" $F
