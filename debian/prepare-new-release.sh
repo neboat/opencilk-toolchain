c343e88896ee0b153d1bf56ef01dcdc9d713c66e
Sylvestre Ledru <sylvestre@debian.org>
2021-07-28 18:43:33 +0200

    update of the bump version script to 14

--- debian/prepare-new-release.sh
+++ debian/prepare-new-release.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-ORIG_VERSION=12
-TARGET_VERSION=13
-ORIG_VERSION_2=12_0
-TARGET_VERSION_2=13_0
-ORIG_VERSION_3=120
-TARGET_VERSION_3=130
+ORIG_VERSION=13
+TARGET_VERSION=14
+ORIG_VERSION_2=13_0
+TARGET_VERSION_2=14_0
+ORIG_VERSION_3=130
+TARGET_VERSION_3=140
 
 LIST=`ls debian/control debian/orig-tar.sh debian/rules debian/patches/clang-analyzer-force-version.diff debian/patches/clang-format-version.diff debian/patches/python-clangpath.diff debian/patches/scan-build-clang-path.diff debian/patches/lldb-libname.diff debian/patches/fix-scan-view-path.diff debian/patches/lldb/lldb-addversion-suffix-to-llvm-server-exec.patch debian/patches/clang-tidy-run-bin.diff debian/patches/fix-scan-view-path.diff debian/README debian/patches/clang-analyzer-force-version.diff debian/patches/clang-tidy-run-bin.diff debian/tests/control debian/unpack.sh debian/tests/cmake-test debian/patches/scan-build-py-fix-analyze-path.diff debian/patches/scan-build-py-fix-default-bin.diff`
 for F in $LIST; do
