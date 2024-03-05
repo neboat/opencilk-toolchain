a403fa2c96f7373b176f1739a92a47d07036d6ec
Sylvestre Ledru <sylvestre@debian.org>
2024-03-05 23:20:25 +0100

    update of the repack llvm ir => .a file

--- debian/llvm-compile-lto-elf.sh
+++ debian/llvm-compile-lto-elf.sh
@@ -25,18 +25,25 @@ NCPUS=$NJOBS
 check_convert_bitcode () {
   local file_name=$(realpath ${1})
   local file_type=$(file ${file_name})
+
   shift
   CLANG_FLAGS="$@"
 
   if [[ "${file_type}" == *"LLVM IR bitcode"* ]]; then
-    # check for an indication that the bitcode was
-    # compiled with -flto
-    ${P_TO_LLVM}/debian/tmp/usr/bin/llvm-bcanalyzer-${VERSION} -dump ${file_name} | grep -xP '.*\-flto((?!-fno-lto).)*' 2>&1 > /dev/null
-    if [ $? -eq 0 ]; then
-      echo "Compiling LLVM bitcode file ${file_name}."
-      ${P_TO_LLVM}/debian/tmp/usr/bin/clang-${VERSION} -fno-lto -opaque-pointers -Wno-unused-command-line-argument \
-        -x ir ${file_name} -c -o ${file_name}
-    fi
+    # Check the output of llvm-strings for the command line, which is in the LLVM bitcode because
+    # we pass -frecord-gcc-switches.
+    # Check for a line that has "-flto" after (or without) "-fno-lto".
+    llvm-strings ${file_name} | while read line ; do
+      flto=$(echo $line   | grep -o -b -e -flto     | tail -n 1 | cut -d : -f 1)
+      fnolto=$(echo $line | grep -o -b -e -fno-lto  | tail -n 1 | cut -d : -f 1)
+
+      if test -n "$flto" && { test -z "$fnolto" || test "$flto" -gt "$fnolto"; } ; then
+        echo "Compiling LLVM bitcode file ${file_name}."
+        clang ${CLANG_FLAGS} -fno-lto -Wno-unused-command-line-argument \
+          -x ir ${file_name} -c -o ${file_name}
+        break
+      fi
+      done
   elif [[ "${file_type}" == *"current ar archive"* ]]; then
     echo "Unpacking ar archive ${file_name} to check for LLVM bitcode components."
     # create archive stage for objects
@@ -47,7 +54,7 @@ check_convert_bitcode () {
     for archived_file in $(find -not -type d); do
       check_convert_bitcode ${archived_file} ${CLANG_FLAGS}
       echo "Repacking ${archived_file} into ${archive}."
-      ${P_TO_LLVM}/debian/tmp/usr/bin/llvm-ar-${VERSION} r ${archive} ${archived_file}
+      ar r ${archive} ${archived_file}
     done
     popd
   fi
@@ -55,5 +62,7 @@ check_convert_bitcode () {
 
 echo "Checking for LLVM bitcode artifacts"
 export -f check_convert_bitcode
-find "$P_TO_LLVM/debian/" -type f -name "*.[ao]" -print0 | \
-  xargs -0 -r -n1 -P$NCPUS bash -c "check_convert_bitcode \$@ $CLANG_FLAGS" ARG0
+# Deduplicate by device:inode to avoid processing hardlinks in parallel.
+find "$P_TO_LLVM/debian/" -type f -name "*.[ao]" -printf "%D:%i %p\n" | \
+  awk '!seen[$1]++' | cut -d" " -f2- | \
+  xargs -d"\n" -r -n1 -P$NCPUS sh -c "check_convert_bitcode \$@ $CLANG_FLAGS" ARG0
