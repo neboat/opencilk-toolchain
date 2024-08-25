ef71807bd47159fad9e13cd36256a066c6299ee9
TB Schardl <neboat@mit.edu>
2024-08-25 10:43:17 -0400

    Initial changes to package OpenCilk compiler toolchain.

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -23,7 +23,7 @@ if test -n "${JENKINS_HOME}"; then
     EXPORT_PATH="$HOME/"
 fi
 
-GIT_BASE_URL=https://github.com/llvm/llvm-project
+GIT_BASE_URL=https://github.com/OpenCilk/opencilk-project
 GIT_TOOLCHAIN_CHECK=https://github.com/opencollab/llvm-toolchain-integration-test-suite.git
 
 reset_repo ()
@@ -32,10 +32,10 @@ reset_repo ()
     git clean -qfd
     git checkout .
     git remote update > /dev/null
-    git reset --hard origin/main > /dev/null
+    git reset --hard origin > /dev/null
     git clean -qfd
-    git checkout main > /dev/null
-    git pull
+    git checkout $2 > /dev/null
+    # git pull
     cd -
 }
 
@@ -59,10 +59,10 @@ fi
 cd - &> /dev/null
 echo "MAJOR_VERSION=$MAJOR_VERSION / CURRENT_VERSION=$CURRENT_VERSION"
 if test -n "$1"; then
-# https://github.com/llvm/llvm-project/tree/release/9.x
+# https://github.com/OpenCilk/opencilk-project/tree/release/9.x
 # For example: sh 4.0/debian/orig-tar.sh release/9.x
     BRANCH=$1
-    if ! echo "$1"|grep -q release/; then
+    if ! echo "$1"|grep -q "dev/\|release/"; then
         # The first argument is NOT a branch, means that it is a stable release
         FINAL_RELEASE=true
         EXACT_VERSION=$1
@@ -80,42 +80,43 @@ else
 fi
 
 if test -n "$1" -a -n "$2"; then
-# https://github.com/llvm/llvm-project/releases/tag/llvmorg-9.0.0
+# https://github.com/OpenCilk/opencilk-project/releases/tag/llvmorg-9.0.0
 # For example: sh 4.0/debian/orig-tar.sh 4.0.1 rc3
 # or  sh 9/debian/orig-tar.sh 9.0.0
     TAG=$2
     RCRELEASE="true"
     EXACT_VERSION=$1
+    BRANCH=$TAG
 fi
 
 # Update or retrieve the repo
 mkdir -p git-archive
 cd git-archive
-if test -d $EXPORT_PATH/llvm-project; then
-    echo "Updating repo in $EXPORT_PATH/llvm-project"
+if test -d $EXPORT_PATH/opencilk-project; then
+    echo "Updating repo in $EXPORT_PATH/opencilk-project"
     # Update it
-    reset_repo $EXPORT_PATH/llvm-project
+    reset_repo $EXPORT_PATH/opencilk-project $BRANCH
 else
     # Download it
-    echo "Cloning the repo in $EXPORT_PATH/llvm-project"
-    git clone $GIT_BASE_URL $EXPORT_PATH/llvm-project
+    echo "Cloning the repo in $EXPORT_PATH/opencilk-project"
+    git clone $GIT_BASE_URL $EXPORT_PATH/opencilk-project
 fi
 
 if test -d $EXPORT_PATH/llvm-toolchain-integration-test-suite; then
     echo "Updating repo in $EXPORT_PATH/llvm-toolchain-integration-test-suite"
     # Update it
-    reset_repo $EXPORT_PATH/llvm-toolchain-integration-test-suite
+    reset_repo $EXPORT_PATH/llvm-toolchain-integration-test-suite main
 else
     echo "Clone llvm-toolchain-integration-test-suite into $EXPORT_PATH/llvm-toolchain-integration-test-suite"
     git clone $GIT_TOOLCHAIN_CHECK $EXPORT_PATH/llvm-toolchain-integration-test-suite
 fi
 
-cd $EXPORT_PATH/llvm-project
+cd $EXPORT_PATH/opencilk-project
 if test -z  "$TAG" -a -z "$FINAL_RELEASE"; then
     # Building a branch
     git checkout $BRANCH
     git reset --hard origin/$BRANCH
-    if test $BRANCH != "main"; then
+    if test $BRANCH != "main"; then # -a $( ! echo "$BRANCH"|grep -q dev/; echo $? ) -eq 0; then
         VERSION=$(echo $BRANCH|cut -d/ -f2|cut -d. -f1)
         if ! echo "$MAJOR_VERSION"|grep -q "$VERSION"; then
             echo "mismatch in version: Dir=$MAJOR_VERSION Provided=$VERSION"
@@ -144,11 +145,12 @@ else
         echo "Mismatch in version: Dir=$MAJOR_VERSION Provided=$EXACT_VERSION"
         exit 1
     fi
-    git_tag="llvmorg-$EXACT_VERSION"
+    # git_tag="llvmorg-$EXACT_VERSION"
     VERSION=$EXACT_VERSION
     if test -n "$TAG"; then
-        git_tag="$git_tag-$TAG"
-        VERSION="$VERSION~+$TAG"
+        git_tag="$TAG"
+        OPENCILK_VERSION_FILENAME=$(echo "$TAG" | sed -rne "s,/,-,p")
+        VERSION="$VERSION~+$OPENCILK_VERSION_FILENAME"
     fi
 
     git checkout "$git_tag" > /dev/null
@@ -161,15 +163,15 @@ rm -rf */www/ build/ build-llvm/
 cd ../
 BASE="llvm-toolchain-${MAJOR_VERSION}_${VERSION}"
 FILENAME="${BASE}.orig.tar.xz"
-cp -R llvm-toolchain-integration-test-suite llvm-project/integration-test-suite
+cp -R llvm-toolchain-integration-test-suite opencilk-project/integration-test-suite
 # Argument to compress faster (for the cost of time)
 export XZ_OPT="-4 -T$(nproc)"
 echo "Compressing to $FILENAME"
-time tar Jcf $CURRENT_PATH/"$FILENAME" --exclude .git --exclude .gitattributes --exclude .git-blame-ignore-revs --exclude .gitignore --exclude .github --exclude build-llvm --transform="s|llvm-project|$BASE|" -C $EXPORT_PATH llvm-project
-rm -rf llvm-project/integration-test-suite
+time tar Jcf $CURRENT_PATH/"$FILENAME" --exclude .git --exclude .gitattributes --exclude .git-blame-ignore-revs --exclude .gitignore --exclude .github --exclude build-llvm --transform="s|opencilk-project|$BASE|" -C $EXPORT_PATH opencilk-project
+rm -rf opencilk-project/integration-test-suite
 
-export DEBFULLNAME="Sylvestre Ledru"
-export DEBEMAIL="sylvestre@debian.org"
+export DEBFULLNAME="Tao B. Schardl"
+export DEBEMAIL="contact@opencilk.org"
 cd "$PATH_DEBIAN"
 
 if test -z "$DISTRIBUTION"; then
--- debian/unpack.sh
+++ debian/unpack.sh
@@ -1,6 +1,6 @@
 set -e
 ORIG_VERSION=19
-MAJOR_VERSION=19 # 8.0.1
+MAJOR_VERSION=19.1.7 # 8.0.1
 REV=`ls -1 *${ORIG_VERSION}_${MAJOR_VERSION}*~+*xz | tail -1|perl -ne 'print "$1\n" if /~\+(.*)\.orig/;'  | sort -ru`
 
 VERSION=$REV
@@ -14,7 +14,8 @@ echo "unpack of $LLVM_ARCHIVE"
 tar Jxf $LLVM_ARCHIVE
 cd llvm-toolchain-${ORIG_VERSION}_$MAJOR_VERSION~+$VERSION/
 
-VER_FOUND=$(grep "LLVM_VERSION_MAJOR " cmake/Modules/LLVMVersion.cmake|awk '{print $2}'|cut -d\) -f1)
+# VER_FOUND=$(grep "LLVM_VERSION_MAJOR " cmake/Modules/LLVMVersion.cmake|awk '{print $2}'|cut -d\) -f1)
+VER_FOUND="$(grep -oP 'set\(\s*LLVM_VERSION_(MAJOR|MINOR|PATCH)\s\K[0-9]+' cmake/Modules/LLVMVersion.cmake | paste -sd '.')"
 if test "${MAJOR_VERSION}" != "$VER_FOUND" -a "${MAJOR_VERSION}.0.0" != "$VER_FOUND" -a "${MAJOR_VERSION}.0.0git" != "$VER_FOUND" -a "${MAJOR_VERSION}git" != "$VER_FOUND"; then
     echo "Mismatch of version"
     echo "Expected $MAJOR_VERSION / Found $VER_FOUND"
